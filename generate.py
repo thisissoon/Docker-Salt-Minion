59dcc3873c89a879804a1d38a1b428c6129113de
Tom Parker <palfrey@tevp.net>
2015-10-06 11:01:54 +0100

    Generate multiple versions of Salt Minion from PPA

---
+++ generate.py
@@ -0,0 +1,11 @@
+from jinja2 import Template
+from os import mkdir, link
+from os.path import exists
+
+template = Template(open('Dockerfile.template').read())
+for version in ["2014-7", "2015-5"]:
+	if not exists(version):
+		mkdir(version)
+	open("%s/Dockerfile" % version, "w").write(template.render(version = version))
+	if not exists("%s/run.sh" % version):
+		link("run.sh", "%s/run.sh" % version)
