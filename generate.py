67ddb905dc11c3b76794d67d1fef066fd1e344cc
Tom Parker <palfrey@tevp.net>
2015-10-06 11:43:58 +0100

    Redo templated build using Salt bootstrap

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from os import mkdir, link
 from os.path import exists
 
 template = Template(open('Dockerfile.template').read())
-for version in ["2014-7", "2015-5"]:
+for version in ["2014.7", "2015.5", "2015.8"]:
 	if not exists(version):
 		mkdir(version)
 	open("%s/Dockerfile" % version, "w").write(template.render(version = version))
